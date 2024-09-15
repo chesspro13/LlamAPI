e50f04ebc88dd8ab11f3f0391fa08612bd1eb2d2
chesspro13 <chesspro13@gmail.com>
2024-09-15 08:37:17 -0700

    Added CORS, updated AI prompt, and got it working!

--- src/api.ts
+++ src/api.ts
@@ -8,14 +8,36 @@ export const router = Router();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const model = new LlamaModel({
-    modelPath: path.join(__dirname, "../models", "capybarahermes-2.5-mistral-7b.Q4_K_M.gguf")
+    modelPath: path.join(__dirname, "../models", "capybarahermes-2.5-mistral-7b.Q4_K_M.gguf"),
+    gpuLayers: 128
 });
-const context = new LlamaContext({model});
-const session = new LlamaChatSession({context});
 
-router.get("/status", async (req: Request, res: Response) => {
-    const a1 = await session.prompt("What is the second letter of the alphabet?").then(a => {
-        console.log(a); 
-        res.send( {result: "success", message: a} )
+function prompt( userPackage: string){
+    return `<|im_start|>system
+        Narrative Statements are a narrative style used to communicate accomplishments and results in the United States Air Force. They should be efficient and increase clarity of an Airman's performance.
+        In the United States Air Force, Narrative Statements should be a standalone sentence with action and at least one of impact or results/outcome and written in plain language without uncommon acronyms and abbreviations.
+        The first word of a narrative statement should be a strong action verb.
+        The performance statement should be one sentence and written in past tense. It should also include transition words like "by" and "which".
+        Personal pronouns (I, me, my, we, us, our, etc.) should not be used.
+        Rewrite the USER prompt to follow these conventions. 
+        Generate Three seporate and unique ways to rewrite what you were given in JSON format labled "V1", "V2", and "V3".
+        <|im_end|>
+        <|im_start|>user
+        ` + userPackage + `
+        <|im_end|>
+        <|im_start|>assistant`
+}
+
+router.post("/status", (req: Request, res: Response) => {
+    const context = new LlamaContext({model});
+    const session = new LlamaChatSession({context});
+    console.log( req.body.package )
+    session.prompt(prompt(req.body.package)).then(a => {
+        console.log("\n\nGenerated Text: [" + a + "]")
+        var cleanJson = "{" + a.toString().split("{")[1].split("}")[0] + "}";
+        cleanJson = cleanJson.replaceAll("\\n","").replaceAll("\n","")
+        cleanJson = cleanJson.replaceAll("\\","").replaceAll(/\s+/g,' ').trim();
+        console.log("Clean JSON: [" + cleanJson + "]")
+        res.send( {result: "success", message: cleanJson }  )
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import { router as apiRouter } from "./api.js";
 import { config } from "dotenv";
+import cors from "cors";
 
 // Environment Variables
 config();
 const API_PORT = process.env.API_PORT || 27415;
 
+const allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5173']
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+  };
+
 // API
 const api = express();
 api.use(express.json());
+api.use(cors(options));
+
 api.use("/api", apiRouter);
 api.listen(API_PORT, () => {
     console.log("API server listening on port: ", API_PORT);
